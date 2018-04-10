52057e2dbc0ac54dc9692501fa8358c8f63f8318
Makoto Tanabe <xekid78@outlook.com>
2018-04-10 17:54:16 +0900

    1st commit

---
+++ ccalconv.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var syowa: Int
+    for (seireki in 1926..1935) {
+        print("西暦${seireki}年は、")
+        syowa = seireki - 1925
+        println("昭和${syowa}年です。")
+    }
+}
